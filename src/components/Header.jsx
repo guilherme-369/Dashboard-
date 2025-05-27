d56c00bfc2c3311107beff29c604c8eb3528c0d7
guilherme <guilherme.dev36@gmail.com>
2025-05-27 15:56:06 -0300

    feat: make header responsive for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,46 @@
-import {Typography, Box, useTheme } from "@mui/material"
+import { Typography, Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 
-const Header = ({title, subTitle}) =>{
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode)
-    return(
-        <Box mb="30px">
-            <Typography
-                variant="h2"
-                color ={colors.grey[100]}
-                fontWeight="bold"
-                sx={{m: "0 0 5px 0"}}
-            >
-                {title}
-            </Typography>
-            <Typography variant="h5" color={colors.greenAccent[400]}>
-                {subTitle}
-            </Typography>
-        </Box>
-    )
-}
+const Header = ({ title, subTitle }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{
+          m: "0 0 5px 0",
+          fontSize: {
+            xs: "1.8rem", // mobile
+            sm: "2.2rem", // tablet (small)
+            md: "2.5rem", // tablet (big)
+            lg: "3rem", // desktop
+            xl: "3.5rem",
+          },
+        }}
+      >
+        {title}
+      </Typography>
+      <Typography
+        variant="h5"
+        color={colors.greenAccent[400]}
+        sx={{
+          fontSize: {
+            xs: "1rem",
+            sm: "1.2rem",
+            md: "1.3rem",
+            lg: "1.5rem",
+            xl: "1.8rem",
+          },
+        }}
+      >
+        {subTitle}
+      </Typography>
+    </Box>
+  );
+};
 
 export default Header;
